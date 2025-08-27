6433d11ff910818499805bb89a2a33102aa101d3
maucodev <hola@maucodev.com>
2025-08-27 14:22:04 -0600

    Handle form submission

--- packages/client/src/components/ChatBox.tsx
+++ packages/client/src/components/ChatBox.tsx
@@ -1,19 +1,50 @@
 import { FaArrowUp } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
 
 import { Button } from './ui/button';
 
+type FormData = {
+    prompt: string;
+};
+
 const ChatBox = () => {
+    const { register, handleSubmit, reset, formState } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        data.prompt = data.prompt.trim();
+        console.log(data);
+        reset();
+    };
+
+    const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            handleSubmit(onSubmit)();
+        }
+    };
+
     return (
-        <div className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl">
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            onKeyDown={onKeyDown}
+            className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+        >
             <textarea
+                {...register('prompt', {
+                    required: true,
+                    validate: (data) => data.trim().length > 0,
+                })}
                 className="w-full border-0 focus:outline-0 resize-none"
                 placeholder="Ask anything"
                 maxLength={1000}
             />
-            <Button className="rounded-full w-9 h-9 cursor-pointer">
+            <Button
+                disabled={!formState.isValid}
+                className="rounded-full w-9 h-9 cursor-pointer"
+            >
                 <FaArrowUp />
             </Button>
-        </div>
+        </form>
     );
 };
 
