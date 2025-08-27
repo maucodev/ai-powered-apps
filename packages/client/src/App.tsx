d8809ea776f9abaf22675ec482a6d0dacc5e768a
maucodev <hola@maucodev.com>
2025-08-27 14:02:11 -0600

    Build the ChatBot component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from 'react';
-import { Button } from './components/ui/button';
+import ChatBox from './components/ChatBox';
 
 function App() {
-    const [message, setMessage] = useState('');
-
-    useEffect(() => {
-        fetch('/api/hello')
-            .then((response) => response.json())
-            .then((data) => setMessage(data.message));
-    }, []);
-
     return (
         <div className="p-4">
-            <p className="font-bold text-3xl">{message}</p>
-            <Button>Click me!</Button>
+            <ChatBox />
         </div>
     );
 }
