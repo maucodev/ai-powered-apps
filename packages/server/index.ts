24257aa4c927d431b6d7f8733edca4e4309ab747
maucodev <hola@maucodev.com>
2025-08-27 12:41:36 -0600

    Extract routes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { chatController } from './controllers/chat.controller';
+import router from './routes';
 
 dotenv.config();
 
@@ -8,8 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post('/api/chat', chatController.sendMessage);
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
