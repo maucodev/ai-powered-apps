5ebcfb566ba79a4ac5a4cab5d7c5947b87f72d08
Mauricio Estrada <hola@maucodev.com>
2026-01-03 12:09:22 -0600

    Displaying loading skeletons

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+import 'react-loading-skeleton/dist/skeleton.css';
 import './index.css';
+
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
